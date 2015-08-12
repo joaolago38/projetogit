43e2ce89cc215fab1868db0803eae04202f23c58
joaohoracio38 <joaolago35@gmail.com>
2015-08-11 21:49:55 -0300

    classe

--- ProjetoGit/src/br/com/model/Carros.java
+++ ProjetoGit/src/br/com/model/Carros.java
@@ -1,5 +1,22 @@
 package br.com.model;
 
 public class Carros {
+	private String nomeCarro;
+	private String placaCarro;
 
+	public String getNomeCarro() {
+		return nomeCarro;
+	}
+
+	public void setNomeCarro(String nomeCarro) {
+		this.nomeCarro = nomeCarro;
+	}
+
+	public String getPlacaCarro() {
+		return placaCarro;
+	}
+
+	public void setPlacaCarro(String placaCarro) {
+		this.placaCarro = placaCarro;
+	}
 }
