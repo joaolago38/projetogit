bae3ec03396a188ab0e7741164418d577b2dc397
joaohoracio38 <joaolago35@gmail.com>
2015-08-11 22:04:06 -0300

    criacao das classes tipoCarros e atributos na classe

--- ProjetoGit/src/br/com/model/Carros.java
+++ ProjetoGit/src/br/com/model/Carros.java
@@ -1,8 +1,27 @@
 package br.com.model;
 
+import java.util.Date;
+
 public class Carros {
 	private String nomeCarro;
 	private String placaCarro;
+    private Date dataFabricacao;
+	private String placa;
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
 
 	public String getNomeCarro() {
 		return nomeCarro;
